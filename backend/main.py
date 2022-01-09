e4dfd6c89f20a3788cddb72da0cea474138e649e
Alan Gardner <urfolomeus@gmail.com>
2022-01-09 23:23:11 +0000

    Making it async

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,26 @@ from PIL import Image
 import config
 import inference
 
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+import time
+
+
+async def generate_remaining_models(models, image, name: str):
+    executor = ProcessPoolExecutor()
+    event_loop = asyncio.get_event_loop()
+    await event_loop.run_in_executor(
+        executor, partial(process_image, models, image, name)
+    )
+
+
+def process_image(models, image, name: str):
+    for model in models:
+        output, _ = inference.inference(models[model], image)
+        name = name.split(".")[0]
+        name = f"{name.split('_')[0]}_{models[model]}.jpg"
+        cv2.imwrite(name, output)
 
 app = FastAPI()
 
@@ -19,13 +39,17 @@ def read_root():
 
 
 @app.post("/{style}")
-def get_image(style: str, file: UploadFile = File(...)):
+async def get_image(style: str, file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
     model = config.STYLES[style]
-    output, resized = inference.inference(model, image)
+    start = time.time()
+    output, _ = inference.inference(model, image)
     name = f"/storage/{str(uuid.uuid4())}.jpg"
     cv2.imwrite(name, output)
-    return {"name": name}
+    models = config.STYLES.copy()
+    del models[style]
+    asyncio.create_task(generate_remaining_models(models, image, name))
+    return {"name": name, "time": time.time() - start}
 
 
 if __name__ == "__main__":
--- frontend/main.py
+++ frontend/main.py
@@ -2,6 +2,8 @@ import requests
 import streamlit as st
 from PIL import Image
 
+import time
+
 STYLES = {
     "candy": "candy",
     "composition 6": "composition_vii",
@@ -34,4 +36,23 @@ if st.button("Style Transfer"):
         img_path = res.json()
         image = Image.open(img_path.get("name"))
         st.image(image, width=500)
+
+        displayed_styles = [style]
+        displayed = 1
+        total = len(STYLES)
+
+        st.write("Generating other models...")
+
+        while displayed < total:
+            for style in STYLES:
+                if style not in displayed_styles:
+                    try:
+                        path = f"{img_path.get('name').split('.')[0]}_{STYLES[style]}.jpg"
+                        image = Image.open(path)
+                        st.image(image, width=500)
+                        time.sleep(1)
+                        displayed += 1
+                        displayed_styles.append(style)
+                    except:
+                        pass
 
