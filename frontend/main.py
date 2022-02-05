47d246f85018c2a4cd525a739340e5a311d7d2ea
Alan Gardner <urfolomeus@gmail.com>
2022-02-05 21:12:28 +0000

    Explain frontend

--- frontend/main.py
+++ frontend/main.py
@@ -31,25 +31,35 @@ style = st.selectbox("Choose the style", [i for i in STYLES.keys()])
 # displays a button
 if st.button("Style Transfer"):
     if image is not None and style is not None:
+        # grab the file and make the request
         files = {"file": image.getvalue()}
         res = requests.post(f"http://backend:8080/{style}", files=files)
+        
+        # use the response to get the first transformed image
         img_path = res.json()
         image = Image.open(img_path.get("name"))
         st.image(image, width=500)
 
+        # record the style that we've got so far
         displayed_styles = [style]
         displayed = 1
         total = len(STYLES)
 
         st.write("Generating other models...")
 
+        # loop until we have the rest of the files async transformed 
         while displayed < total:
             for style in STYLES:
                 if style not in displayed_styles:
                     try:
+                        # get the image
                         path = f"{img_path.get('name').split('.')[0]}_{STYLES[style]}.jpg"
                         image = Image.open(path)
+
+                        # display it
                         st.image(image, width=500)
+
+                        # pause for a second then record the image that we juts displayed
                         time.sleep(1)
                         displayed += 1
                         displayed_styles.append(style)
